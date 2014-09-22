320920e2f13c70c90b003546d296ab3ebf45fc1c
Christophe Braud <christophe.braud@aerolitec.com>
2014-09-22 14:35:20 +0200

    - Create template for UIView class

--- File Templates/Aerolitec/Objective-C class.xctemplate/UIView/___FILEBASENAME___.m
+++ File Templates/Aerolitec/Objective-C class.xctemplate/UIView/___FILEBASENAME___.m
@@ -1,8 +1,9 @@
 //
-//  ___FILENAME___
-//  ___PROJECTNAME___
+// ___FILENAME___
+// ___PROJECTNAME___
 //
-//  Created by ___FULLUSERNAME___ on ___DATE___.
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// Base on Aerolitec Template
 //___COPYRIGHT___
 //
 
@@ -13,31 +14,172 @@
 #pragma mark -
 
 #pragma mark -> Class Extension
-@interface ___FILEBASENAMEASIDENTIFIER___ ()
+@interface ___FILEBASENAMEASIDENTIFIER___ () { // <<#protocol name#>Delegate, <#protocol name#>Delegate, <#protocol name#>Delegate> {
+	//#pragma mark -> Member variables for private properties
+	//<#type#> *<#member name#>;
+	//<#type#>  <#member name#>;
+  
+	//#pragma mark -> Member variables for public properties
+	//<#type#> *<#member name#>;
+	//<#type#>  <#member name#>;
+}
+
+//#pragma mark -> Private properties
+//
+//@property (nonatomic,strong) <#type#> *<#private property#>;
+//@property (nonatomic,assign) <#type#>  <#private property#>;
+
+//#pragma mark -> Private class methods
+//
+//+ (void)<#private class method#>();
+//+ (NSInteger)<#private class method#>();
+//+ (CGFloat)<#private class method#>();
+//+ (NSString *)<#private class method#>();
+
+//#pragma mark -> Private methods
+//
+//- (void)<#private method#>();
+//- (NSInteger)<#private method#>();
+//- (CGFloat)<#private method#>();
+//- (NSString *)<#private method#>();
 
 @end
 
 #pragma mark -> Implementation
+
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
-#pragma mark -> Init/deallocate methods
+//#pragma mark -> Private properties
 
+//@synthesize <#private property#>;
+//@synthesize <#private property#>;
+//@synthesize <#private property#>;
 
-- (id)initWithFrame:(CGRect)frame {
-	self = [super initWithFrame:frame];
+//#pragma mark -> Public properties
+
+//@synthesize <#public property#>;
+//@synthesize <#public property#>;
+//@synthesize <#public property#>;
+
+//#pragma mark -> Initialized class
+
+//+ (void)initialize {
+//  if (self == [<#class name#> class]) {
+//    <#statements#>
+//  }
+//}
+
+//#pragma mark -> Private class methods
+
+//+ (void)<#private class method#>() {
+//}
+
+//+ (NSInteger)<#private class method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//+ (CGFloat)<#private class method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//+ (NSString *)<#private class method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Public class methods
+
+//+ (void)<#public class method#>() {
+//}
+
+//+ (NSInteger)<#public class method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//+ (CGFloat)<#public class method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//+ (NSString *)<#public class method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+#pragma mark -> Init/Deallocate methods
+
+- (instancetype)init {
+	self = [super init];
 	if (self) {
-		// Initialization code
+		//<#statements#>
 	}
 	return self;
 }
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect
- {
- // Drawing code
- }
- */
+- (instancetype)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	if (self) {
+		//<#statements#>
+	}
+	return self;
+}
+
+
+//#pragma mark -> Private methods
+
+//- (void)<#private method#>() {
+//}
+
+//- (NSInteger)<#private method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#private method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//- (NSString *)<#private method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Public methods
+
+//- (void)<#public method#>() {
+//}
+
+//- (NSInteger)<#public method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#public method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//- (NSString *)<#public method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Class override UIView
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+//  - (void)drawRect:(CGRect)rect
+//  {
+//  // Drawing code
+//  }
+
+//#pragma mark -> Class override <#class name#>
+
+//#pragma mark -> Protocol <#protocol name#>
 
 @end
+
