08486934f9f31e926468520a3a52aba0ee2f44f8
Christophe Braud <christophe.braud@aerolitec.com>
2015-01-12 10:38:49 +0100

    - Remove some files not used as Aerolitec Templates - Add template for class base on JSONModel used as a struct

---
+++ File Templates/Aerolitec/Objective-C class.xctemplate/JSONModel/___FILEBASENAME___.h
@@ -0,0 +1,34 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Base on Aerolitec Template
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+#pragma mark -
+#pragma mark ___FILEBASENAMEASIDENTIFIER___
+#pragma mark -
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+//@property (nonatomic, strong) <#type#> *<#property name#>;
+//@property (nonatomic, assing) <#type#>  <#property name#>;
+
+//#pragma mark -> Public class methods (optional)
+
+//#pragma mark -> Public Init methods (optional)
+
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#>
+
+//#pragma mark -> Public methods (optional)
+
+//- (void)<#public method#>();
+//- (NSInteger)<#public method#>();
+//- (CGFloat)<#public method#>();
+//- (NSString *)<#public method#>();
+
+@end
---
+++ File Templates/Aerolitec/Objective-C class.xctemplate/JSONModel/___FILEBASENAME___.m
@@ -0,0 +1,75 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Base on Aerolitec Template
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+#pragma mark -
+#pragma mark ___FILEBASENAMEASIDENTIFIER___
+#pragma mark -
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+//#pragma mark -> Private class methods (optional)
+
+//#pragma mark -> Public class methods (optional)
+
+//#pragma mark -> Private Dealloc method (optional)
+
+//- (void)dealloc {
+//  self.<#property#> = nil;
+//  self.<#property#> = nil;
+//}
+
+//#pragma mark -> Public Init methods (optional)
+
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#> {
+//	self = [super init];
+//	if (self) {
+//		//<#statements#>
+//	}
+//	return self;
+//}
+
+//- (id)initWithStyle:(UITableViewCellStyle)pStyle reuseIdentifier:(NSString *)pReuseIdentifier {
+//	self = [super initWithStyle:pStyle reuseIdentifier:pReuseIdentifier];
+//	if (self) {
+//		//<#statements#>
+//	}
+//	return self;
+//}
+
+//#pragma mark -> Public methods (optional)
+
+//- (void)<#public method#>() {
+//}
+
+//- (NSInteger)<#public method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#public method#>() {
+//  CGFloat lRet = 0.0f;
+//  return lRet;
+//}
+
+//- (NSString *)<#public method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Class override JSONModel
+
+//+(JSONKeyMapper *)keyMapper {
+//  JSONKeyMapper *lRet = [[JSONKeyMapper alloc] initWithDictionary:@{@"<#JSON field#>":@"<#Internal field#>"}];
+//   
+//  return lRet;
+//}
+
+@end
