de81a5de099d6ff0e470c1cc6fdab578716d8474
Christophe Braud <christophe.braud@aerolitec.com>
2015-04-17 11:32:59 +0200

    - Define template for UICollectionViewCell and UICollectionViewController

---
+++ File Templates/Aerolitec/Objective-C class.xctemplate/UICollectionViewController/___FILEBASENAME___.h
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Base on Aerolitec Template
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+//#pragma mark -> Public properties
+//
+//@property (nonatomic,strong) <#type#> *<#public property#>;
+//@property (nonatomic,assign) <#type#>  <#public property#>;
+
+//#pragma mark -> Public class methods
+//
+//+ (void)<#public class method#>();
+//+ (NSInteger)<#public class method#>();
+//+ (CGFloat)<#public class method#>();
+//+ (NSString *)<#public class method#>();
+
+//#pragma mark -> Public Init methods
+//
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#>;
+
+//#pragma mark -> Public methods
+//
+//- (void)<#public method#>();
+//- (NSInteger)<#public method#>();
+//- (CGFloat)<#public method#>();
+//- (NSString *)<#public method#>();
+
+@end
---
+++ File Templates/Aerolitec/Objective-C class.xctemplate/UICollectionViewController/___FILEBASENAME___.m
@@ -0,0 +1,370 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Base on Aerolitec Template
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+//#pragma mark -
+//#pragma mark <#custom cell name#>Cell
+//#pragma mark -
+
+//@interface <#custom cell name#>Cell : UICollectionViewCell
+
+//@property (weak, nonatomic) IBOutlet <#type#>    *<#name#>;
+//@property (weak, nonatomic) IBOutlet <#type#>    *<#name#>;
+//@property (weak, nonatomic) IBOutlet <#type#>    *<#name#>;
+
+//#pragma mark -> Public class methods (optional)
+
+//#pragma mark -> Public Init  (optional)
+
+//#pragma mark -> Public methods (optional)
+
+//@end
+
+//@implementation <#custom cell name#>Cell
+
+//#pragma mark -> Private class methods (optional)
+
+//#pragma mark -> Public class methods (optional)
+
+//#pragma mark -> Private Deallocate method (optional)
+
+//- (void)dealloc {
+//  self.<#property#> = nil;
+//  self.<#property#> = nil;
+//}
+
+//#pragma mark -> Public Init methods (optional)
+
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#> {
+//	self = [super init];
+//	if (self) {
+//		//<#statements#>
+//	}
+//	return self;
+//}
+
+//#pragma mark -> Public methods (optional)
+
+//- (void)<#public method#>() {
+//}
+
+//- (NSInteger)<#public method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#public method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//- (NSString *)<#public method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+//@end
+
+#pragma mark -
+#pragma mark ___FILEBASENAMEASIDENTIFIER___
+#pragma mark -
+
+#pragma mark -> Class Extension
+@interface ___FILEBASENAMEASIDENTIFIER___ () { // <<#protocol name#>Delegate,<#protocol name#>Delegate,<#protocol name#>Delegate> {
+  //#pragma mark -> Member variables for private properties
+  //<#type#> *<#member name#>;
+  //<#type#>  <#member name#>;
+  
+  //#pragma mark -> Member variables for public properties
+  //<#type#> *<#member name#>;
+  //<#type#>  <#member name#>;
+}
+
+//#pragma mark -> Private properties
+//
+//@property (nonatomic,strong) <#type#> *<#private property#>;
+//@property (nonatomic,assign) <#type#>  <#private property#>;
+
+//#pragma mark -> Private class methods
+//
+//+ (void)<#private class method#>();
+//+ (NSInteger)<#private class method#>();
+//+ (CGFloat)<#private class method#>();
+//+ (NSString *)<#private class method#>();
+
+//#pragma mark -> Private Init/Dealloc methods
+//
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#>;
+//- (void)dealloc;
+
+//#pragma mark -> Private methods
+//
+//- (void)<#private method#>();
+//- (NSInteger)<#private method#>();
+//- (CGFloat)<#private method#>();
+//- (NSString *)<#private method#>();
+
+//#pragma mark -> Notification methods
+//
+//- (void)notification<#notification name#>:(NSNotification *)pNotification;
+
+//#pragma mark -> Interface Builder properties
+//
+//@property (weak, nonatomic) IBOutlet <#type#> *<#IB property#>;
+//@property (weak, nonatomic) IBOutlet <#type#> *<#IB property#>;
+//@property (weak, nonatomic) IBOutlet <#type#> *<#IB property#>;
+
+//#pragma mark -> Interface Builder actions
+//
+//- (IBAction)<#IB Action method#>:(id)sender;
+//- (IBAction)<#IB Action method#>:(id)sender;
+//- (IBAction)<#IB Action method#>:(id)sender;
+
+@end
+
+#pragma mark -> Implementation
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+//#pragma mark -> Private properties
+
+//@synthesize <#private property#>;
+//@synthesize <#private property#>;
+//@synthesize <#private property#>;
+
+//#pragma mark -> Public properties
+
+//@synthesize <#public property#>;
+//@synthesize <#public property#>;
+//@synthesize <#public property#>;
+
+//#pragma mark -> Initialized class
+
+//+ (void)initialize {
+//  if (self == [<#class name#> class]) {
+//    <#statements#>
+//  }
+//}
+
+//#pragma mark -> Private class methods
+
+//+ (void)<#private class method#>() {
+//}
+
+//+ (NSInteger)<#private class method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//+ (CGFloat)<#private class method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//+ (NSString *)<#private class method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Public class methods
+
+//+ (void)<#public class method#>() {
+//}
+
+//+ (NSInteger)<#public class method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//+ (CGFloat)<#public class method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//+ (NSString *)<#public class method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Private Init/Deallocate methods
+
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#> {
+//	self = [super init];
+//	if (self) {
+//		//<#statements#>
+//	}
+//	return self;
+//}
+
+//- (void)dealloc {
+//  self.<#property#> = nil;
+//  self.<#property#> = nil;
+//}
+
+//#pragma mark -> Public Init methods
+
+//- (instancetype)initWith<#param#>:(<#type#>)p<#param#> {
+//	self = [super init];
+//	if (self) {
+//		//<#statements#>
+//	}
+//	return self;
+//}
+
+//- (id)initWithStyle:(UITableViewStyle)pStyle {
+//  self = [super initWithStyle:pStyle];
+//  if (self) {
+//		//<#statements#>
+//  }
+//  return self;
+//}
+
+//#pragma mark -> Private methods
+
+//- (void)<#private method#>() {
+//}
+
+//- (NSInteger)<#private method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#private method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//- (NSString *)<#private method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Public methods
+
+//- (void)<#public method#>() {
+//}
+
+//- (NSInteger)<#public method#>() {
+//  NSInteger lRet = 0;
+//  return lRet;
+//}
+
+//- (CGFloat)<#public method#>() {
+//  CGFloat lRet = 0;
+//  return lRet;
+//}
+
+//- (NSString *)<#public method#>() {
+//  NSString * lRet = nil;
+//  return lRet;
+//}
+
+//#pragma mark -> Class override <#class name#>
+
+#pragma mark -> Class override UICollectionViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  //Uncomment the following line to preserve selection between presentations.
+  //self.clearsSelectionOnViewWillAppear = NO;
+  
+  //Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+  //self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  //Dispose of any resources that can be recreated.
+}
+
+//#pragma mark -> Navigation
+
+//- (void)prepareForSegue:(UIStoryboardSegue *)pSegue sender:(id)pSender {
+//  if ([pSegue.identifier isEqualToString:@"segue<#custom segue name#>"]) {
+//    <#class#>ViewController *l<#class#>ViewController = [pSegue destinationViewController];
+//    l<#class#>ViewController.<#public property#> = <#value#>;
+//  } else if ([pSegue.identifier isEqualToString:@"segue<#custom segue name#>"])   {
+//    <#class#>ViewController *l<#class#>ViewController = [pSegue destinationViewController];
+//    l<#class#>ViewController.<#public property#> = <#value#>;
+//  }
+//}
+
+//#pragma mark -> Notifications
+
+//- (void)prepareForSegue:(UIStoryboardSegue *)pSegue sender:(id)pSender {
+//  if ([pSegue.identifier isEqualToString:@"segue<#custom segue name#>"]) {
+//    <#class#>ViewController *l<#class#>ViewController = [pSegue destinationViewController];
+//    l<#class#>ViewController.<#public property#> = <#value#>;
+//  } else if ([pSegue.identifier isEqualToString:@"segue<#custom segue name#>"])   {
+//    <#class#>ViewController *l<#class#>ViewController = [pSegue destinationViewController];
+//    l<#class#>ViewController.<#public property#> = <#value#>;
+//  }
+//}
+
+//#pragma mark -> Actions Implementation
+
+//- (IBAction)clickOn<#action#>:(id)pSender {
+//}
+
+//#pragma mark -> Protocol <#protocol name#>
+
+#pragma mark Protocol UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)pCollectionView {
+#warning Incomplete method implementation.
+  return 0;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)pCollectionView numberOfItemsInSection:(NSInteger)pSection {
+#warning Incomplete method implementation.
+  return 0;
+}
+
+//- (UICollectionViewCell *)collectionView:(UICollectionView *)pCollectionView cellForItemAtIndexPath:(NSIndexPath *)pIndexPath {
+//  UICollectionViewCell *lCell = [collectionView dequeueReusableCellWithReuseIdentifier:<#@"reuseIdentifier"#> forIndexPath:pIndexPath];
+//
+//  // Configure the cell
+//
+//  return lCell;
+//}
+
+#pragma mark Protocol UICollectionViewDelegate
+
+//- (BOOL)collectionView:(UICollectionView *)pCollectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)pIndexPath {
+//  return YES;
+//}
+
+//- (BOOL)collectionView:(UICollectionView *)pCollectionView shouldSelectItemAtIndexPath:(NSIndexPath *)pIndexPath {
+//  return YES;
+//}
+
+//- (BOOL)collectionView:(UICollectionView *)pCollectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)pIndexPath {
+//  return NO;
+//}
+
+//- (BOOL)collectionView:(UICollectionView *)pCollectionView canPerformAction:(SEL)pAction forItemAtIndexPath:(NSIndexPath *)pIndexPath withSender:(id)pSender {
+//  return NO;
+//}
+
+//- (void)collectionView:(UICollectionView *)pCollectionView performAction:(SEL)pAction forItemAtIndexPath:(NSIndexPath *)pIndexPath withSender:(id)pSender {
+//}
+
+- (void)collectionView:(UICollectionView *)pCollectionView didSelectItemAtIndexPath:(NSIndexPath *)pIndexPath {
+}
+
+//#pragma mark -> Protocol UICollectionViewDelegateFlowLayout
+//
+//- (CGSize)collectionView:(UICollectionView *)pCollectionView layout:(UICollectionViewLayout *)pCollectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)pIndexPath {
+//  CGSize lRet = CGSizeMake(<#Width#>, <#Height#>);
+//
+//  return lRet;
+//}
+
+@end
